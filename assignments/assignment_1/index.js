70e8ca64e56f8044c336a1ea4c3a37d2df36e47f
RAMYA <kandagatlaramya5@gmail.com>
2021-12-28 12:53:48 +0530

    assignment-1 commit

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,17 +1,35 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    var name=process.argv[2];
+    return name
 }
 
 function getNameFromEnv() {
     // Write your code here
+    process.env.ENV_VARIABLE=getNameFromCommandLine();
+    return process.env.ENV_VARIABLE
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline=require("readline");
+    const rl=readline.createInterface({
+        input:process.stdin,
+        output:process.stdout
+
+    });
+    rl.question("enter the name?", (answer) =>{
+        console.log("Hello",answer);
+    });
+
+
 }
 
-module.exports = {
+module.exports={
     getNameFromCommandLine,
     getNameFromEnv,
-    getNameFromReadLine
+    getNameFromReadLine,
 }
+    
+
+    
